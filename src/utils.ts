d665c48269b5c7ffad35f0887d083abd9593cc18
Eduard Tesvintsev <tesvintsev@gmail.com>
2023-10-06 22:16:51 +0300

    React Router

--- src/utils.ts
+++ src/utils.ts
@@ -1,31 +1,22 @@
-export const categoryIds = {
+export const categoryIds: { [index: string]: number } = {
   index: 0,
-
-  fashion: 3,
-
-  technologies: 1,
-
-  politics: 4,
-
   sport: 2,
+  technologies: 1,
+  politics: 4,
+  fashion: 3,
 };
 
-export const categoryNames = {
+export const categoryNames: { [index: string]: string } = {
   index: 'Главная',
-
   fashion: 'Мода',
-
   technologies: 'Технологии',
-
-  politics: 'Политика',
-
   sport: 'Спорт',
+  politics: 'Политика',
 };
 
 export const beautifyDate = (date: string): string => {
   return new Date(date).toLocaleDateString('ru-RU', {
     month: 'long',
-
     day: 'numeric',
   });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,34 @@
-//webpack.config.js
-
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // плагин, необходимый для подстановки в файл index.html скрипта сгенерированного webpack
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const EslintWebpackPlugin = require('eslint-webpack-plugin');
-const StylelintWebpackPlugin = require('stylelint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
-  entry: { main: './src/index.tsx' }, // точка входа - указывает путь к основному скрипту проекта, отыскав его, вебпак выстроит дерево зависимостей и раскрутит все импорты по цепочке
+  mode: process.env.NODE_ENV || 'production',
+  entry: './src/script.tsx',
   output: {
-    // точка куда должен выгружаться конечный результат работы. Путь должен быть абсолютным, для это пользуемся встроенной в node утилитой path
-    path: path.resolve(__dirname, 'dist'), // выстраиваем путь к папке и записываем имя папки куда выгружаем
-    filename: 'main.[contenthash].js', // имя конечного файла
-  },
-  mode: process.env.NODE_ENV, // режим разработчика. Оптимизирует процесс сборки, когда мы отлаживаем свой проект.
-  devtool: 'source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, '/.dist'),
-    },
-    compress: true,
-    port: 8080,
-    open: true,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
   },
   module: {
     rules: [
-      // rules — это массив правил
-      // добавим в него объект правил для бабеля
       {
-        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader его нужно установить в проект через npm i babel-loader
         use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: '/node_modules/', // раздел исключений - включаем сюда директории через которые не хотим чтобы проходил babel-loader
+        exclude: /node_modules/,
       },
       {
-        // регулярное выражение, которое ищет все файлы с такими расширениями
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource', // asset/resource управляет статичными файлами - графика, шрифты, переносит их из исходников в финальную сборку проекта и подставляет правильные пути к ним
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
       },
       {
         test: /\.(ts|tsx)$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
-      {
-        // применять это правило только к CSS-файлам
-        test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // style-loader и css-loader Устанавливаем их в проект npm i css-loader style-loader --save-dev
-        use: ['style-loader', 'css-loader'],
       },
     ],
   },
@@ -62,13 +39,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    new StylelintWebpackPlugin({
-      files: '{**/*,*}.css',
+    new StylelintPlugin({
+      files: 'src/{**/*,*}.css',
     }),
-    new EslintWebpackPlugin({
-      files: '{**/*,*}.{tsx,ts,js}',
+    new ESLintPlugin({
+      files: 'src/{**/*,*}.{tsx,ts}',
     }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
   ],
+  devServer: {
+    open: true,
+  },
 };
