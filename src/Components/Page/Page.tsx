2fc2f2cb438003244c531419fbd708f2d52ee875
Eduard Tesvintsev <tesvintsev@gmail.com>
2023-10-27 22:38:22 +0300

    Madal-dropdown

--- src/Components/Page/Page.tsx
+++ src/Components/Page/Page.tsx
@@ -1,19 +1,33 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment, useState } from 'react';
 import './Page.css';
 import { Navigation } from '../Navigation/Navigation';
 import { Logo } from '../Logo/Logo';
 import { ColorSchemeSwitcher } from '@components/ColorSchemeSwitcher/ColorSchemeSwitcher';
+import { EmailModal } from '@components/EmailModal/EmailModal';
+
+const LS_EMAIL_SHOWN_KEY = 'newsfeed:email_modal_shown';
 
 export const Page: FC = ({ children }) => {
+  const [emailModalShown, setEmailModalShown] = useState(!!localStorage.getItem(LS_EMAIL_SHOWN_KEY));
   return (
     <Fragment>
+      {emailModalShown && (
+        <EmailModal
+          onClose={() => {
+            localStorage.setItem(LS_EMAIL_SHOWN_KEY, 'true');
+            setEmailModalShown(false);
+          }}
+        >
+          Hello
+        </EmailModal>
+      )}
       <header className="header">
         <div className="container header__container">
           <Logo />
           <Navigation className="header__navigation" />
-            <div className="header__controls">
-                <ColorSchemeSwitcher />
-            </div>
+          <div className="header__controls" style={{ transform: 'translateX(0)' }}>
+            <ColorSchemeSwitcher />
+          </div>
         </div>
       </header>
 
@@ -26,14 +40,14 @@ export const Page: FC = ({ children }) => {
             <Navigation className="footer__navigation" />
           </div>
           <div className="footer__bottom">
-              © 2023 Новостная лента сделана{' '}
-              <a className="footer__link" href="https://tesvintsevDev.github.io" target="_blank" rel="noreferrer">
-                TES.inc{' '}
-              </a>
-              при поддержке{' '}
-              <a href="https://karpov.courses/frontend" target="_blank" rel="noreferrer" className="footer__link">
-                Karpov.Courses
-              </a>              
+            © 2023 Новостная лента сделана{' '}
+            <a className="footer__link" href="https://tesvintsevDev.github.io" target="_blank" rel="noreferrer">
+              TES.inc{' '}
+            </a>
+            при поддержке{' '}
+            <a href="https://karpov.courses/frontend" target="_blank" rel="noreferrer" className="footer__link">
+              Karpov.Courses
+            </a>
           </div>
         </div>
       </footer>
